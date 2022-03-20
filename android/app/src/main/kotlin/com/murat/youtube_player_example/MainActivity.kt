8968899a3897f41ce7a8e7d8626352c5d07072a6
miyigun2017 <miyigun2017@gmail.com>
2022-03-20 22:27:25 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/murat/youtube_player_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.murat.youtube_player_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+void main() {
+  runApp(Programim());
+}
+
+class Programim extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Youtube Oynatıcı',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AnaSayfa(title: 'Youtube Oynatıcı'),
+    );
+  }
+}
+
+class AnaSayfa extends StatefulWidget {
+  AnaSayfa({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _AnaSayfaDurum createState() => _AnaSayfaDurum();
+}
+
+class _AnaSayfaDurum extends State<AnaSayfa> {
+  YoutubePlayerController _controller = YoutubePlayerController(
+      initialVideoId: 'TD9hkjlPICE', // id youtube video
+      flags: YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 30,
+              width: 200,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.cyan,
+                    Colors.green,
+                  ],
+                ),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    //spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  'Youtube video',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.blueGrey,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            InkWell(
+              onTap: () {
+                _showDialog();
+              },
+              child: Container(
+                height: 70,
+                width: 250,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.pink,
+                      Colors.purple,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      //spreadRadius: 5,
+                      blurRadius: 5,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      "90 lar Türkçe Pop videosunu izlemek için tıkla!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _showDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: true, //sadece butonlara basınca ekrandan çıkıyor
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          backgroundColor: Colors.greenAccent,
+          title: Text(
+            "Youtube Player",
+            textAlign: TextAlign.center,
+          ),
+          actions: <Widget>[
+            YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.blueAccent,
+            )
+          ],
+        );
+      },
+    );
+  }
+}
